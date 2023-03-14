7d3b4a3370766b35edaf1847b29e833a9d3e0393
Ahsan Suny <ehsan.puc16@gmail.com>
2023-03-14 11:28:32 +0100

    TurboPWM added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <ADS131M08.h>
-
+#include <SAMD21turboPWM.h>
 
 //Ahsan@Schneider
 #define CS_PIN 5
@@ -16,12 +16,23 @@ unsigned long long sampleNum = 0;
 unsigned long long currentMicros = 0;
 char outputarr[128];
 
+
+TurboPWM pwm;
+const int ADC_CLK2 = 3;   //       8 is the pin number of mkr board
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   adc.init();
 
   delay(100);
+
+
+
+  pwm.setClockDivider(1, true);     // Input clock is divided by 1 and sent to Generic Clock, Turbo is On
+  pwm.timer(1, 1, 47, true);       // Timer 1 is set to Generic Clock divided by 1, resolution is 250, normal aka fast aka single-slope PWM
+  pwm.analogWrite(ADC_CLK2, 500);
+
 
   pinMode(RESET, OUTPUT);
   digitalWrite(RESET, LOW);
@@ -81,6 +92,8 @@ void setup() {
   delay(1000);
 
   currentMicros = micros();
+
+
 }
 
 
