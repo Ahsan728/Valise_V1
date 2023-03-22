b9b9b88a923d32ecfee930f88cc9618338e7b1d8
Ahsan Suny <ehsan.puc16@gmail.com>
2023-03-22 10:42:45 +0100

    UPD

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,10 @@ const int ADC_CLK2 = 8;   //       8 is the pin number of mkr board
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pwm.setClockDivider(1, true);     // Input clock is divided by 1 and sent to Generic Clock, Turbo is On
+  pwm.timer(2, 1, 47, true);       // Timer 1 is set to Generic Clock divided by 1, resolution is 250, normal aka fast aka single-slope PWM
+  pwm.analogWrite(ADC_CLK2, 500);
+
   adc.init();
 
   delay(100);
